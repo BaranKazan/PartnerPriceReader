822ce211405dc3f67dd2553d88c1a1915dc17996
Baran Kazan <Baran.kazan@hotmail.com>
2024-06-10 02:02:49 +0200

    Removed unused code

--- src/main/java/com/trivago/casestudy/PriceController.java
+++ src/main/java/com/trivago/casestudy/PriceController.java
@@ -1,7 +1,5 @@
 package com.trivago.casestudy;
 
-import java.util.List;
-
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
